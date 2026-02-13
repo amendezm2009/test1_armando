4d1543351b3293b465de7e392ef4b7033159953b
Armando Méndez <amendezm2009@gmail.com>
2026-02-12 19:30:37 -0600

    Add README and initial index.php file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # test1_armando
 repositorio para mostrar mis skills trabajando con git
+
+## Institución educativa
+Universidad Tecnológica de la Selva
+
+## Lugar de procedencia
+Ocosingo, Chiapas. 2026
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Armando's Page</title>
+</head>
+<body>
+    <h1>
+        Bienvenido a la página de Armando, 
+        un apasionado de la tecnología y el desarrollo web. 
+    </h1>
+</body>
+</html>
